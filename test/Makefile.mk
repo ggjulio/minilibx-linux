9da4a2bab79820487a9972f5f0971aecd613cde6
ggjulio <juligonz@student.42.fr>
2021-03-17 22:02:16 +0100

    .

--- test/Makefile.mk
+++ test/Makefile.mk
@@ -6,19 +6,18 @@ INCLIB=$(INC)/../lib
 
 CC=gcc
 
-CFLAGS= -I$(INC) -O3 -I..
+CFLAGS= -I$(INC) -O3 -I.. -g
 
 NAME= mlx-test
 SRC = main.c
-OBJ = $(SRC:.c=.o)
+OBJ = $(SRC:%.c=%.o)
 
-all	:$(NAME)
+all: $(NAME)
 
-$(NAME)	:$(OBJ)
+$(NAME): $(OBJ)
 	$(CC) -o $(NAME) $(OBJ) -L.. -lmlx -L$(INCLIB) -lXext -lX11 -lm -lbsd
 
-clean	:
+clean:
 	rm -f $(NAME) $(OBJ) *~ core *.core
 
-
-re	: clean all
+re: clean all
